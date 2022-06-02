714c36323c6455be8f77ddf66e911e192e56e550
jkryst <j.kryst@outlook.com>
2022-06-02 13:42:52 +0200

    initial commit

---
+++ src/zad1/Calc.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Kryst Jakub PD3724
+ *
+ */
+
+package zad1;
+
+
+public class Calc {
+}  
---
+++ src/zad2/Purchase.java
@@ -0,0 +1,11 @@
+/**
+ *
+ *  @author Kryst Jakub PD3724
+ *
+ */
+
+package zad2;
+
+
+public class Purchase {
+}  
