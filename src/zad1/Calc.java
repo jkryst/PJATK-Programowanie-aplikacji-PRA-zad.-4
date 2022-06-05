aa8ddce30f9a9a553caa88357ff25b6ce375e6f2
jkryst <j.kryst@outlook.com>
2022-06-06 00:48:25 +0200

    update

--- src/zad1/Calc.java
+++ src/zad1/Calc.java
@@ -6,82 +6,93 @@
 
 package zad1;
 
-import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
+import java.math.MathContext;
+import java.util.HashMap;
 
-public class Calc implements PropertyChangeListener {
-private PropertyChangeSupport chg = new PropertyChangeSupport(this);
-String op;	
+public class Calc  {
 
-	
-synchronized void setOp(String newOp) {
-	
-	String oldOp = op;
-	op = newOp;
-	propertyChange.firePropertyChange("count", new String(oldOp),
-            new String(newOp));
-	
-}
+	public MathContext mc = new MathContext(7);
+	public HashMap<String, Method> metody = new HashMap<String, Method>();
 	
 	
-public String doCalc(String s) {
+public Calc() {
 	
-	String arg[] = s.split(" +");
-	BigDecimal a = new BigDecimal(arg[0]);
-	BigDecimal b = new BigDecimal(arg[2]);	
-	op = arg[1];
-	
-
-	BigDecimal wyn = a.multiply(b);
+	try {
 		
-	String wynik = wyn.toString();
-	return wynik;
-}
+		Class<?> klasa = Class.forName("java.math.BigDecimal");
+	    Method dod = klasa.getMethod("add",BigDecimal.class);
+	    metody.put("+", dod);
+	    
+	    Method odejm = klasa.getMethod("subtract",BigDecimal.class);
+	    metody.put("-", odejm);
+	    
+	    Method mnoz = klasa.getMethod("multiply",BigDecimal.class);
+	    metody.put("*", mnoz);
+	    
+	    Method dziel = klasa.getMethod("divide",BigDecimal.class, MathContext.class);
+	    metody.put("/", dziel);
 
-
-
-
-
-
-public BigDecimal sum(BigDecimal a, BigDecimal b) {
-	return a.add(b);
+	} catch(ClassNotFoundException | NoSuchMethodException e) {}
+	
 	
 }
-
-
-public BigDecimal substract(BigDecimal a, BigDecimal b) {
-	return a.subtract(b);
 	
-}
-
-
-public BigDecimal multiply(BigDecimal a, BigDecimal b) {
-	return a.multiply(b);
 	
-}
-
-
-public BigDecimal divide(BigDecimal a, BigDecimal b) {
-	return a.divide(b);
+public String doCalc(String cmd) {
 	
-}
+	try {
+	
+	String s[] = cmd.split(" +");
+	BigDecimal a = new BigDecimal(s[0]);
+	BigDecimal b = new BigDecimal(s[2]);	
+	Method dzialanie = metody.get(s[1]);
+	
+    try {
 
+	 BigDecimal wynik = (BigDecimal) dzialanie.invoke(a, b);
+		return wynik.toString();
+	
 
-public synchronized void addPropertyChangeListener(PropertyChangeListener l) {
-	 chg.addPropertyChangeListener(l);
-	 }
-
-public synchronized void removePropertyChangeListener(PropertyChangeListener l) {
-	 chg.removePropertyChangeListener(l);
-	 }
-
-
-
-@Override
-public void propertyChange(PropertyChangeEvent evt) {
-	// TODO Auto-generated method stub
+    } catch (NumberFormatException e ) {
+		
+		return "Invalid command to calc";
+		
+	} catch (IllegalAccessException e) {
+		
+		return "Invalid command to calc";
+		
+	} catch (IllegalArgumentException e) {
+		
+		BigDecimal wynik;
+		
+		try {
+			
+			wynik = (BigDecimal) dzialanie.invoke(a, b, mc);
+			return wynik.toString();
+			
+		} catch (IllegalAccessException e1) {
+			return "Invalid command to calc";
+			
+		} catch (IllegalArgumentException e1) {
+			return "Invalid command to calc";
+			
+		} catch (InvocationTargetException e1) {
+			return "Invalid command to calc";
+			
+		}
+		
+	
+	} catch (InvocationTargetException e) {
+		
+		return "Invalid command to calc";
+	} 
+    
+	} catch (NumberFormatException e) {
+		return "Invalid command to calc";
+	}
 	
 }
 
